ae9b72ad562af027a69ad7b3e6c65b9c53840e89
David Ryder <david@wildreason.com>
2012-05-23 14:39:38 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+G5devsite::Application.config.secret_token = 'c796f4ca693977d72661d609763ee574a07dd3c3865cb93776f8c8609a7d8f08e8ecf70b22ba650f74ba506f7099179ebc553a4185236f240bb15346653e44a0'
