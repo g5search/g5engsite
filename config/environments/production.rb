0a4898c8eb8215d1d2d098bba18a11d57616dc12
David Ryder <david@libryder.com>
2012-06-15 11:05:43 -0700

    fuck the asset pipeline

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,6 +54,9 @@ G5devsite::Application.configure do
   # Enable threaded mode
   # config.threadsafe!
 
+  # Do not compress assets
+  config.assets.compress = false
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
