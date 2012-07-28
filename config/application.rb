2741eb7b8480e079030eea2c66a8d221ea99acef
David Ryder <david@libryder.com>
2012-07-28 00:11:15 -0700

    fixing asset precompile issue on heroku

--- config/application.rb
+++ config/application.rb
@@ -39,6 +39,8 @@ module G5devsite
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.assets.initialize_on_precompile = false
+    
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
